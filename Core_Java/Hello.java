a2f4bbc8223f62dc448e25fd059760ee3c58d8ab
subhash1599 <subhashnimmala01@gmail.com>
2024-01-17 21:19:15 +0530

    Data Types and Variable Declaration

---
+++ Core_Java/Hello.java
@@ -0,0 +1,25 @@
+public class Hello{
+
+    public static void main(String a[]){
+        int num=3;
+        byte by=127; // limit up to 127 only
+        short sh=558;
+        long l=5854l;
+
+        float f=5.8f;
+        double d=5.8;
+
+        char c='k';
+
+        boolean b=true;
+        
+        
+        System.out.println("Hello World"); 
+        System.out.println(num);
+        System.out.println(b);
+
+
+
+
+    }
+}
